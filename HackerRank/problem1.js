34bce9fd84643869f329a95ee172538b55e8951d
tilakchandghimire <136582697+tilakchandghimire@users.noreply.github.com>
2023-11-06 10:31:35 -0500

    Update problem1.js

--- HackerRank/problem1.js
+++ HackerRank/problem1.js
@@ -1,1 +1,8 @@
-
+//hackerank problem
+//factorial function declaration with one parameter that returns value
+function fact(a){
+  if (a>=1 && a<=5){
+    return a * fact(a-1);
+  }
+  return 1;
+}
